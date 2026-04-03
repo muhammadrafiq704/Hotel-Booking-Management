bb6efcd0b352ea9b34ead6cef61f78d8e177fd35
muhammadrafiq704 <sameerbalti704@gmail.com>
2026-04-03 17:25:07 +0500

    add waiting text

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ function App() {
       <h1 style={{ textAlign: 'center', marginTop: '50px', fontSize: '36px' }}>Hotel Booking Management</h1>
       <h1 style={{ textAlign: 'center', marginTop: '10px', fontSize: '24px', color:'gray' }}>Coming Soon!</h1>
       <h1 style={{ textAlign: 'center', marginTop: '10px', fontSize: '16px', color:'gray' }}>hotel booking management !</h1>
+      <h1 style={{ textAlign: 'center', marginTop: '10px', fontSize: '10px', color:'gray' }}>waiting !</h1>
     </>
   )
 }
