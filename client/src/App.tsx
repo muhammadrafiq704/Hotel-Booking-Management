a4c6cab0bd4c21c9dbaf926a8c8e516d8eeb49f6
muhammadrafiq704 <sameerbalti704@gmail.com>
2026-04-04 15:46:07 +0500

    feat: added new setup and project initialization

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,45 +1,10 @@
+import './App.css'
+
 function App() {
-	return (
-		<>
-			<h1 style={{ textAlign: "center", marginTop: "50px", fontSize: "36px" }}>
-				Hotel Booking Management
-			</h1>
-			<h1
-				style={{
-					textAlign: "center",
-					marginTop: "10px",
-					fontSize: "24px",
-					color: "gray",
-				}}
-			>
-				Coming Soon!
-			</h1>
-			<h1
-				style={{
-					textAlign: "center",
-					marginTop: "10px",
-					fontSize: "16px",
-					color: "gray",
-				}}
-			>
-				hotel booking management !
-			</h1>
-			<h1
-				style={{
-					textAlign: "center",
-					marginTop: "10px",
-					fontSize: "10px",
-					color: "gray",
-				}}
-			>
-				waiting !
 
-
-
-			</h1>
-			<span>biome lefthook and conventional commits setup!  bad commit   </span>
-		</>
-	);
+  return (
+    <h1 style={{fontSize:'36px', fontWeight:700}}>HOTEL BOOKING MANAGEMENT</h1>
+  )
 }
 
-export default App;
+export default App
--- client/src/components/Button.tsx
+++
@@ -1,5 +0,0 @@
-const Button = () => {
-	return <div>hi form button</div>;
-};
-
-export default Button;
